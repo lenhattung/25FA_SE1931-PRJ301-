216eb2a61874d14b84a555ce1081f7f3335218e1
lenhattung <lenhattung@gmail.com>
2025-09-15 10:21:20 +0700

    u
    u

---
+++ Project 03b_Login_ChuyenTrang/src/java/model/User.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author tungi
+ */
+public class User {
+    private String userName;
+    private String password;
+    private String fullName;
+    private String role;
+    private boolean active;
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    public boolean login(String userName, String password){
+        return userName.equalsIgnoreCase("admin")&&password.equals("123456");
+    }
+    
+}
